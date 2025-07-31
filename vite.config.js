f407d89972553e5f315c7cdad8a9d1d6cbe08704
goldnik2020-dot <goldnik2020@gmail.com>
2025-07-31 12:21:47 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,3 +8,10 @@ export default defineConfig({
     outDir: 'dist'
   }
 });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/travelgenie-ai1/' // 👈 This must match your GitHub repo name
+})
