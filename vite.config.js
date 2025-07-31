0472149a6b76d445ef76c12cef07a925de0417c4
goldnik2020-dot <goldnik2020@gmail.com>
2025-07-31 14:34:55 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,3 @@ export default defineConfig({
   plugins: [react()],
   base: '/travelgenie-ai1/',
 });
- import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/travelgenie-ai1/', // GitHub Pages subpath
-});
